f8b403db937b8fb1aa797edb8aa93e63a510d52a
Simon Montasir Adnan <114659070+SimonMAdnan@users.noreply.github.com>
2025-11-01 18:07:51 +0000

    Added the items to the admin pannel

--- Assignment1/transport_api/admin.py
+++ Assignment1/transport_api/admin.py
@@ -1,3 +1,72 @@
 from django.contrib import admin
+from transport_api.models import Stop, Route, Vehicle, Agency, Calendar, Trip, StopTime, Shape, SpatialQuery
 
-# Register your models here.
+
+@admin.register(Stop)
+class StopAdmin(admin.ModelAdmin):
+    list_display = ('stop_id', 'stop_name', 'stop_code', 'stop_type')
+    search_fields = ('stop_id', 'stop_name', 'stop_code')
+    list_filter = ('stop_type', 'wheelchair_boarding', 'created_at')
+    ordering = ('stop_name',)
+
+
+@admin.register(Route)
+class RouteAdmin(admin.ModelAdmin):
+    list_display = ('route_id', 'route_short_name', 'route_long_name', 'route_type', 'operator')
+    search_fields = ('route_id', 'route_short_name', 'route_long_name')
+    list_filter = ('route_type', 'operator')
+    ordering = ('route_short_name',)
+
+
+@admin.register(Agency)
+class AgencyAdmin(admin.ModelAdmin):
+    list_display = ('agency_id', 'agency_name', 'agency_url', 'agency_timezone')
+    search_fields = ('agency_id', 'agency_name')
+    ordering = ('agency_name',)
+
+
+@admin.register(Calendar)
+class CalendarAdmin(admin.ModelAdmin):
+    list_display = ('service_id', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday')
+    search_fields = ('service_id',)
+    list_filter = ('monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday')
+
+
+@admin.register(Trip)
+class TripAdmin(admin.ModelAdmin):
+    list_display = ('trip_id', 'route', 'service_id', 'trip_headsign')
+    search_fields = ('trip_id', 'trip_headsign')
+    list_filter = ('route', 'service_id')
+    ordering = ('trip_id',)
+
+
+@admin.register(StopTime)
+class StopTimeAdmin(admin.ModelAdmin):
+    list_display = ('trip', 'stop', 'arrival_time', 'departure_time', 'stop_sequence')
+    search_fields = ('trip__trip_id', 'stop__stop_name')
+    list_filter = ('stop_sequence', 'trip')
+    ordering = ('trip', 'stop_sequence')
+
+
+@admin.register(Shape)
+class ShapeAdmin(admin.ModelAdmin):
+    list_display = ('shape_id', 'geometry')
+    search_fields = ('shape_id',)
+    list_filter = ('shape_id',)
+    ordering = ('shape_id',)
+
+
+@admin.register(Vehicle)
+class VehicleAdmin(admin.ModelAdmin):
+    list_display = ('vehicle_id', 'route', 'bearing', 'speed', 'status', 'timestamp')
+    search_fields = ('vehicle_id',)
+    list_filter = ('status', 'route', 'timestamp')
+    readonly_fields = ('created_at', 'updated_at')
+    ordering = ('-timestamp',)
+
+
+@admin.register(SpatialQuery)
+class SpatialQueryAdmin(admin.ModelAdmin):
+    list_display = ('query_type', 'created_at')
+    list_filter = ('query_type', 'created_at')
+    readonly_fields = ('created_at',)
